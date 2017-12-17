6758189d56dc0f7eaf548303b25d57f1e85cbae5
Ana-MariaCovaci <anacovaci0509@gmail.com>
2017-12-17 16:59:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Arcade Game Project
+
+## Installation
+Clone or download this repository, and open the index.html file in your browser.
+
+Or [See it live!](https://ana-mariacovaci.github.io/project-arcade-game/)
+
+## How The Game Works
+
+In this game you have a Player(a pretty Princess) and Enemies (Bugs).
+The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down.
+Once the player collides with an enemy, the player moves back to the start square. Once the player reaches the water the game is won.
+
+## How To Play
+Use the keyboard arrows to move the character.
---
+++ js/app.js
@@ -0,0 +1,129 @@
+// Enemies our player must avoid
+class Enemy {
+
+    // Constructor for a new Enemy
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/enemy-bug.png';
+    }
+
+    // Update the enemy's position
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x = this.x + 100 * dt;
+
+        // When the enemy moves off the screen, resets the enemy position
+        if (this.x > 600) {
+            this.x = randomSpawn();
+        }
+
+        // Check for player/enemy collision
+        if (this.x > player.x - 50 &&
+            this.x < player.x + 50 &&
+            this.y > player.y - 50 &&
+            this.y < player.y + 50) {
+                player.x = player.startX;
+                player.y = player.startY;
+        }
+
+        // Logs the enemy x and y position
+        console.log('x: ' + this.x, 'y:' + this.y);
+
+    }
+
+    // Draw the enemy on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
+
+// The Player Class
+class Player {
+
+    // Constructor for a new Player
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-princess-girl.png';
+        this.startX = x;
+        this.startY = y;
+    }
+
+    update() {
+        this.x = this.x;
+        this.y = this.y;
+    }
+
+    // Draw the player on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(controls) {
+       // Movement controls
+        switch (controls) {
+            case 'up':
+                this.y -= 85;
+                break;
+            case 'down':
+                this.y += 85;
+                break;
+            case 'right':
+                this.x += 100;
+                break;
+            case 'left':
+                this.x -= 100;
+                break;
+        }
+
+        // Logs the player x and y position
+        console.log('x: ' + this.x, 'y:' + this.y);
+
+        // Confine player movement
+        if (this.x > 400) {
+            this.x = 400;
+        } else if (this.x < 0) {
+            this.x = 0;
+        } else if (this.y > 390) {
+            this.y = 390;
+        } else if (this.y < 0) {
+            alert("Congratulations!");
+            this.x = this.startX;
+            this.y = this.startY;
+        }
+    }
+
+}
+
+// Returns a random -x location for the enemy to spawn
+function randomSpawn() {
+    let randomLocation = [-100, -150, -200, -250, -300, -350, -400];
+    return randomLocation[Math.floor(Math.random() * randomLocation.length)];
+}
+
+// Array containing the enemy objects
+const allEnemies = [
+    new Enemy(-200, 60),
+    new Enemy(-150, 130),
+    new Enemy(-300, 210),
+    new Enemy(-100, 130)
+];
+
+// Player object
+const player = new Player(200, 390);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
